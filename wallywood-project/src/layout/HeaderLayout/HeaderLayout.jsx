6349c6bacceaf0daed20117c4a1e81f7b68b19e2
simo994q <simonkn1@hotmail.com>
2023-09-11 10:16:54 +0200

    header start

--- wallywood-project/src/layout/HeaderLayout/HeaderLayout.jsx
+++ wallywood-project/src/layout/HeaderLayout/HeaderLayout.jsx
@@ -5,9 +5,26 @@ export const HeaderLayout = () => {
 
     return (
         <div className={style.headerLayoutContainer}>
-        
-        header
-        
+
+
+
+            <div className={style.logo}>
+                <h1>WALLYWOOD</h1>
+            </div>
+
+
+
+            <div className={style.linksAndCart}>
+                <img src="/basket.svg" alt="" />
+                <ul>
+                    <li>w</li>
+                    <li>e</li>
+                    <li>t</li>
+                    <li>e</li>
+                    <li>w</li>
+                </ul>
+            </div>
+
         </div>
     )
 }
