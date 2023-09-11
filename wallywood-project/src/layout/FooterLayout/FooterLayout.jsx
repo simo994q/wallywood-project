e41780ea30e7f3fbfaf413cc80a2f74fe2a507cb
simo994q <simonkn1@hotmail.com>
2023-09-11 11:22:11 +0200

    footer images

--- wallywood-project/src/layout/FooterLayout/FooterLayout.jsx
+++ wallywood-project/src/layout/FooterLayout/FooterLayout.jsx
@@ -6,6 +6,25 @@ export const FooterLayout = () => {
     return (
         <div className={style.footerLayoutContainer}>
             footer
+
+            
+            left side
+            <div>
+                address
+                <div></div>
+                info
+                <div></div>
+            </div>
+
+
+
+            icons
+            <div>
+                <img src="/pinterest.svg" alt="" />
+                <img src="/instagram.svg" alt="" />
+                <img src="/facebook.svg" alt="" />
+                <img src="/twitter.svg" alt="" />
+            </div>
         </div>
     )
 }
