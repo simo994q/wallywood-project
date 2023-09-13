488514b046422433d641cdeb52f47f3a761ff957
simo994q <simonkn1@hotmail.com>
2023-09-13 09:02:10 +0200

    details

--- wallywood-project/src/components/DetailsPage/DetailsPage.jsx
+++ wallywood-project/src/components/DetailsPage/DetailsPage.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react'
 
 export const DetailsPage = () => {
 
-    const {id} = useParams()
+    const { id } = useParams()
 
     let url = `http://localhost:4000/poster/details/${id}`
 
@@ -47,12 +47,16 @@ export const DetailsPage = () => {
                         <img src={data?.image} alt="" />
                         <div className={style.posterDetails}>
                             <h3>{data?.name}</h3>
-                            <p>{data?.description}</p>
+                            <p>{data?.description.length > 0 ? data?.description : 'Beskrivelse ikke tilgængelig'}</p>
                             <p>Størrelse: {data?.width} x {data?.height} cm</p>
                             <p>Varenummer (SKU): {data?.id}</p>
                             <h4>Kr. {data?.price},-</h4>
-                            <div>
-                                input and button and stock
+                            <div className={style.cartAndStock}>
+                                <div>
+                                    <input type="number" className={style.addAmount} defaultValue='0'/>
+                                    <input type="submit" value="Læg i kurv" className={style.addToCart} />
+                                </div>
+                                <p style={{color: data?.stock === 0 ? 'red' : 'green'}}>{data?.stock} på lager</p>
                             </div>
                         </div>
                     </div>
