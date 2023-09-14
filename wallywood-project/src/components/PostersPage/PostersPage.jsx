713114b4452a603bc5e24929d74ad0ca668e599a
simo994q <simonkn1@hotmail.com>
2023-09-14 14:55:51 +0200

    fucking lorte sorterings system mand

--- wallywood-project/src/components/PostersPage/PostersPage.jsx
+++ wallywood-project/src/components/PostersPage/PostersPage.jsx
@@ -5,24 +5,41 @@ import { Filter } from '../Filter/Filter'
 
 export const PostersPage = () => {
 
-    const [url, setUrl] = useState('http://localhost:4000/poster/list')
+    // const [url, setUrl] = useState('http://localhost:4000/poster/list')
 
     const [data, setData] = useState()
 
-    useEffect(() => {
-        fetch(url).then(res => res.json()).then(data => setData(data))
-    }, [url])
+    const [showItem, setShowItem] = useState(6)
+    const [fetchUrl, setFetchUrl] = useState(`http://localhost:4000/poster/list?limit=`)
 
+    // useEffect(() => {
+    //     setFetchUrl(fetchUrl)
+    // }, [showItem])
+
+
+    useEffect(() => {
+        console.log(123);
+        useFetch(fetchUrl)
+    }, [showItem, fetchUrl])
+    
+    const useFetch = (url) => {
+        fetch(url + showItem).then(res => res.json()).then(data => setData(data))
+        console.log(data);
+    }
+    
     const setSort = (sort) => {
         switch (sort) {
             case 'low':
-                setUrl('http://localhost:4000/poster/list?sort_key=price&sort_direction=asc')
+                setShowItem(6)
+                setFetchUrl(`http://localhost:4000/poster/list?sort_key=price&sort_direction=asc&limit=`)
                 break;
             case 'high':
-                setUrl('http://localhost:4000/poster/list?sort_key=price&sort_direction=desc')
+                setShowItem(6)
+                setFetchUrl(`http://localhost:4000/poster/list?sort_key=price&sort_direction=desc&limit=`)
                 break;
             case 'title':
-                setUrl('http://localhost:4000/poster/list?sort_key=name')
+                setShowItem(6)
+                setFetchUrl(`http://localhost:4000/poster/list?sort_key=name&limit=`)
                 break;
         }
     }
@@ -41,21 +58,27 @@ export const PostersPage = () => {
                 </div>
                 <div className={style.filterAndPosters}>
                     <Filter />
-                    <div className={style.posters}>
-                        {data && data.map((item) => {
+                    <div className={style.postersContainer}>
+                        <div className={style.posters}>
+                            {data && data.map((item) => {
 
-                            const itemLink = `/poster/${item.id}`
+                                const itemLink = `/poster/${item.id}`
 
-                            return (
-                                <div key={item.id}>
-                                    <NavLink to={itemLink}><img src={item.image} alt={item.name} /></NavLink>
-                                    <h3><NavLink to={itemLink}>{item.name}</NavLink></h3>
-                                    <p>Kr. {item.price},-</p>
-                                    <input type="button" value="Læg i kurv" />
-                                </div>
-                            )
-                        })}
+                                return (
+                                    <div key={item.id}>
+                                        <NavLink to={itemLink}><img src={item.image} alt={item.name} /></NavLink>
+                                        <h3><NavLink to={itemLink}>{item.name}</NavLink></h3>
+                                        <p>Kr. {item.price},-</p>
+                                        <input type="button" value="Læg i kurv" />
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        <div className={style.showMoreContainer}>
+                            <input type="button" value="Vis flere" onClick={() => setShowItem(showItem + 6)} />
+                        </div>
                     </div>
+
                 </div>
             </div>
         </>
